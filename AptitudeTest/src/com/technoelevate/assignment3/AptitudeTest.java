18039798d0cae3943305355969474638b56fff64
nishmitha17 <84074903+nishmitha17@users.noreply.github.com>
2021-06-04 12:04:03 +0530

    Add files via upload

---
+++ AptitudeTest/src/com/technoelevate/assignment3/AptitudeTest.java
@@ -0,0 +1,14 @@
+package com.technoelevate.assignment3;
+
+import jdk.jfr.Percentage;
+
+public class AptitudeTest {
+
+	public static void main(String[] args) {
+		ValidateQuestion v=new ValidateQuestion();
+		v.displayQuestion();
+		v.result();
+		
+	}
+
+}
